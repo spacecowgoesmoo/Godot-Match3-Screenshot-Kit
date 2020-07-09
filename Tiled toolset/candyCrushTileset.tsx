1b0d81d15c5714a8075cc5c881714461bbd844db
Taylor Calderone <spacecowgoesmoo@gmail.com>
2020-07-09 16:47:03 -0700

    Lots of progress on visuals

--- Tiled toolset/candyCrushTileset.tsx
+++ Tiled toolset/candyCrushTileset.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.1" name="candyCrushSpritesheet" tilewidth="90" tileheight="90" tilecount="40" columns="5">
- <image source="candyCrushSpritesheet.png" width="450" height="806"/>
+ <image source="Images/candyCrushSpritesheet.png" width="450" height="806"/>
 </tileset>
