bf2b35138d51ea2b7a1fc717cced61706978568a
Taylor Calderone <spacecowgoesmoo@gmail.com>
2020-07-09 13:33:55 -0700

    Spritesheet V2

--- Tiled toolset/candyCrushTileset.tsx
+++ Tiled toolset/candyCrushTileset.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.1" name="candyCrushSpritesheet" tilewidth="100" tileheight="100" tilecount="100" columns="10">
- <image source="candyCrushSpritesheet.png" width="1000" height="1000"/>
+<tileset version="1.4" tiledversion="1.4.1" name="candyCrushSpritesheet" tilewidth="90" tileheight="90" tilecount="40" columns="5">
+ <image source="candyCrushSpritesheet.png" width="450" height="806"/>
 </tileset>
